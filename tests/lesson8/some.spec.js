7d7e550bab8680a2302077d1b35d9679b5467087
Makson <xmonomnenado@gmail.com>
2024-08-30 14:54:09 +0300

    modify file some.spec.js

--- tests/lesson8/some.spec.js
+++ tests/lesson8/some.spec.js
@@ -1,22 +1,30 @@
 import {test,expect,chromium} from '@playwright/test';
 
 test ( "some test", async()=> {
-const browser = await chromium.launch();
-const page = await browser.newPage();
-const context = await browser.newContext();
-
-await page.goto ('https://developer.mozilla.org/en-US/docs/MDN/Community/Contributing/Translated_content#active_locales');
-await page.pause()
-await page.locator ('//input[@id="top-nav-search-input"]').click();
-await page.locator ('//input[@id="top-nav-search-input"]').fill('xpath');
-await page.locator ('//button[@type="submit"]').click()
-await page.locator('//h3/a[@href="/en-US/docs/Web/XPath"]').click();
-let SecondElement= await page.locator('//a[@href="/en-US/docs/Web/XPath/Functions"]').nth(1);
-await SecondElement.click();
-let secondElement = await page.locator('//a[@href="/en-US/docs/Web/XPath/Functions/id"]').nth(1);
-await secondElement.click();
-
-await context.close();
-await browser.close();
-} 
+   const browser = await chromium.launch();
+   const context = await browser.newContext();
+   const page = await context.newPage();
+  
+   await page.goto('https://developer.mozilla.org/en-US/docs/MDN/Community/Contributing/Translated_content#active_locales');
+   await page.pause(); 
+   await page.locator('//input[@id="top-nav-search-input"]').waitFor();  
+   await page.locator('//input[@id="top-nav-search-input"]').click();
+   await page.locator('//input[@id="top-nav-search-input"]').fill('xpath');
+   await page.locator('//button[@type="submit"]').waitFor();
+   await page.locator('//button[@type="submit"]').click();
+   await page.waitForLoadState('networkidle');
+   await page.locator('//h3/a[@href="/en-US/docs/Web/XPath"]').click();
+   const secondElement = await page.locator('//a[@href="/en-US/docs/Web/XPath/Functions"]').first();
+   await secondElement.click();
+   const nextElement = await page.locator('//a[@href="/en-US/docs/Web/XPath/Functions/id"]').first();
+   await nextElement.click();
+   await page.locator('//a[@href="/en-US/docs/Web/XPath/Functions/key"]').click();
+   await page.locator('//div[@class="toc-container"]//div[@class="document-toc-container"]//a[@href="#description"]').click();
+   const xslKey = await page.locator('//div[@class="section-content"]//a[@href="/en-US/docs/Web/XSLT/Element/key"]').first();
+   await xslKey.click();
+ 
+   await page.close();
+   await context.close();
+   await browser.close();
+ }
 )
